7d991a6e162acef4c0833416db4280ea283df393
Bill Nguyen <billfl93@gmail.com>
2022-07-28 21:01:13 -0400

    commit tree 7385f61020408f720ba50673cda62c2ec5384a4e

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solutions in this file
+const names = ["Guadalupe", "Ollie", "Aki"];
+
+function writeCards(names) {
+  let thankyouCards = []
+  for (let i=0; i < names.length; i++) {
+    thankyouCards.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`);
+  }
+
+  return thankyouCards;
+}
+
+writeCards(names);
+
+function countDown(startingNumber) {
+  while (startingNumber > 0) {
+    console.log (startingNumber);
+    startingNumber -=1;
+  }
+  console.log(startingNumber);
+}
